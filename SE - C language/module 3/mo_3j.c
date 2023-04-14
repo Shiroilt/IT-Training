4891adc6eebd392ab4d12edc5ac4923c85264039
Shiroilt <123527246+Shiroilt@users.noreply.github.com>
2023-04-14 11:50:20 +0530

    update

--- SE - C language/module 3/mo_3j.c
+++ SE - C language/module 3/mo_3j.c
@@ -1,15 +1,18 @@
 #include<stdio.h>
-void so(int *a[10],int j){
-		int i,s;
-		for(i=0;i<j/2;i++){
-			if(*a[i]>*a[j-i-1]){
-				s=*a[i];
-				*a[i]=*a[j-i-1];
-				*a[j-i-1]=s;
+void so(int a[10],int j){
+		int i,s,k;
+		for(i=0;i<j;i++)
+		{
+		for(k=i+1;k<j;k++){
+			if(a[i]>a[k]){
+				s=a[i];
+				a[i]=a[k];
+				a[k]=s;
 			}
 		}
+		}
 		for(i=0;i<j;i++){
-			printf("%d",*a[i]);		
+			printf("%d ",a[i]);		
 		}
 }
 int main(){
